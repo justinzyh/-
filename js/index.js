5ebe0548e61f2959e1add21fd94d456005635dcb
justinzyh <tojustinzyh@163.com>
2019-03-10 16:31:49 +0800

    图片上传

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function() {
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		centerMode: true,
+		slidesToShow: 1,
+		centerPadding: '120px',
+		autoplay: false,
+		autoplaySpeed: 3000,
+		draggable: false
+	});
+	$('.sliderImg').on('click', function() {
+		var dataId = $(this).attr('data-id');
+		if (dataId == 187 || dataId == 188) {
+			layer.open({
+				type: 2,
+				title: '',
+				shadeClose: false,
+				shade: 0.8,
+				area: ['980px', '598px'],
+				offset: '5px',
+				content: '/index/video?id=' + dataId,
+				maxmin: true,
+				cancel: function(index) {}
+			});
+		}
+	});
+
+});
